97532906144714ace8129958afe4fbea07a67b7d
saiki-iijima <saiiijim@yahoo-corp.jp>
2017-12-21 14:41:13 +0900

    rpcたたけた…

--- main.go
+++ main.go
@@ -4,10 +4,38 @@ import (
 	"net/http"
 
 	"github.com/sasasaiki/eth-page/src/go/router"
+	"log"
+	"bytes"
+	"github.com/gorilla/rpc/v2/json2"
 )
 
 func main() {
 	r := router.CreateRoute(router.NewProdRoutingHandlers())
+
+	resp, err := http.Post("http://localhost:8545/rpc", "application/json",
+		bytes.NewBufferString(`{
+			"jsonrpc": "2.0",
+			"method": "eth_getBalance",
+			"params": ["0x2f7f14f554632786d74320161a6152adf3b6e94d","latest"],
+			"id":1
+		}`))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	var ret interface{}
+	err = json2.DecodeClientResponse(resp.Body, &ret)
+	if err != nil {
+		log.Println("aaa")
+		log.Fatalln(err)
+	}
+
+	log.Println("call")
+	if err != nil {
+		log.Fatalf("call %v",err)
+	}
+	log.Printf("result %#v",ret)
+
+
 	http.ListenAndServe(":8080", r)
-	
 }
